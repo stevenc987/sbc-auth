dcabb9b99ff20eb66166ab0f85c7eb6df5594f75
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2021-04-07 18:11:28 -0700

    Adding exclusion of json file from pre caching

--- auth-web/vue.config.js
+++ auth-web/vue.config.js
@@ -42,7 +42,9 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'src/service-worker.js'
+      swSrc: 'src/service-worker.js',
+      exclude: [/\.json$/]
+
     }
   }
 }
